11ac3606d1d68dfec21e7d39a03a0784773582d9
Ruben Van de Vijver <rvandev@sandia.gov>
2018-10-09 16:57:12 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "PESViewer",
+    version = "1.0",
+    packages = find_packages(),
+    entry_points={'console_scripts':[
+        'pesviewer = PESViewer:main',
+        ]},
+    install_requires=['matplotlib','numpy'],
+    
+    author="Ruben Van de Vijver",
+    author_email = "vandevijver.ruben@gmail.com",
+    
+    description = "Depiction of Potential Energy Surfaces.",
+    license = "MIT",
+    url = "https://github.com/rubenvdvijver/PESViewer.git",
+)
